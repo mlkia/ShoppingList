09adc624bd22f951085c7d905af96ff7fdcb5037
Malik <mlk.2plus@gmail.com>
2022-10-27 21:32:59 +0200

    Add Add item to hme page and create NewListPage in Views

--- ShoppingList/ShoppingList/AppShell.xaml.cs
+++ ShoppingList/ShoppingList/AppShell.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace ShoppingList
+﻿using ShoppingList.Views;
+using Xamarin.Forms;
+
+namespace ShoppingList
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            
+            Routing.RegisterRoute(nameof(NewListPage), typeof(NewListPage));
+
         }
         
     }
--- ShoppingList/ShoppingList/Views/NewListPage.xaml.cs
+++ ShoppingList/ShoppingList/Views/NewListPage.xaml.cs
@@ -10,17 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace ShoppingList.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : ContentPage
+    public partial class NewListPage : ContentPage
     {
-        public HomePage()
+        public NewListPage()
         {
             InitializeComponent();
-        }
-
-        async void OnAddClicked(object sender, EventArgs e)
-        {
-            // Navigate to the NewListPage, without passing any data.
-            await Shell.Current.GoToAsync(nameof(NewListPage));
         }
     }
 }
