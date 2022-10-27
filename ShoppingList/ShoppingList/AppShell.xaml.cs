170cedf6ccc6b3334ebb696a28b0e5b58e28f0db
Malik <mlk.2plus@gmail.com>
2022-10-27 13:25:09 +0200

    Created Home page

--- ShoppingList/ShoppingList/AppShell.xaml.cs
+++ ShoppingList/ShoppingList/AppShell.xaml.cs
@@ -1,23 +1,12 @@
-﻿using ShoppingList.ViewModels;
-using ShoppingList.Views;
-using System;
-using System.Collections.Generic;
-using Xamarin.Forms;
-
-namespace ShoppingList
+﻿namespace ShoppingList
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            
         }
-
-        private async void OnMenuItemClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//LoginPage");
-        }
+        
     }
 }
