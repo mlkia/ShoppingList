bf392e4270a575852c51f06465c5486c85476fac
Malik <mlk.2plus@gmail.com>
2022-11-07 11:34:28 +0100

    Create Models

---
+++ ShoppingList/ShoppingList/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using System.Text;
+using SQLiteNetExtensions.Attributes;
+
+namespace ShoppingList.Models
+{
+    internal class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ItemlId { get; set; }
+
+        public string Text { get; set; }
+        public bool Done { get; set; }
+
+        [ForeignKey(typeof(List))]
+        public int NoteId { get; set; }
+
+        [ManyToOne]
+        public List List { get; set; }
+    }
+}
---
+++ ShoppingList/ShoppingList/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using System.Text;
+using SQLiteNetExtensions.Attributes;
+
+namespace ShoppingList.Models
+{
+    internal class List
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<Item> Item { get; set; }
+    }
+}
